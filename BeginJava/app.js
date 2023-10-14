8d05f070f07a6da8a112a91b5d16adf1fd74428e
Muhammad-Ehtesham <shamiaurangzeb@gmail.com>
2023-10-14 17:48:57 +0500

    Chapter1-4

---
+++ BeginJava/app.js
@@ -0,0 +1,124 @@
+// -------------------------------------Chapter 1--------------------------------------------------------
+// ----------Question 1-------------
+// alert("Welcome To New Begining");
+// ---------------------------------
+
+// ----------Question 2--------------------------
+// alert("Error! Please enter a valid password");
+// ----------------------------------------------
+
+// ----------Question 3--------------------------
+//alert("Welcome to JS Land... \nHappy Coding.");
+// ----------------------------------------------
+
+// ----------Question 4--------------------------
+// alert("Welcom to JS Land...");
+// alert("Happy Coding.");
+// ----------------------------------------------
+
+// ----------Question 5-----------------------------------------
+// console.log("I can run JS through my web Browser's Console");
+// -------------------------------------------------------------
+// -------------------------------------END------------------------------------------------------------------
+
+
+// ------------------------------------Chapter 2-------------------------------------------------------------
+// ----------Question 1----------
+// var username="Ehtesham";
+// alert(username);
+// ------------------------------
+
+// ----------Question 2-----------
+// var myName="Ehtesham Aurngzeb";
+// alert(myName);
+// -------------------------------
+
+// ----------Question 3-----------
+// var message='Hello World';
+// alert(message);
+// -------------------------------
+
+// ---------------Question 4--------------------------------
+// var Name="Ehtesham Aurngzeb";
+// var age=20;
+// var info="Certified Mobile Aplication Development";
+// alert(Name);
+// alert(age +" years old");
+// alert(info);
+// ----------------------------------------------------------
+
+// -----------------Question 5-------------------
+// var text = "Pizza";
+// var output = "";
+// for (var i = text.length; i > 0; i--) {
+//   output += text.slice(0, i) + "\n";
+// }
+// alert(output);
+// ----------------------------------------------
+
+// ---------------Question 6------------------
+// var email="shami@gmail.com";
+// alert("My email address is "+email);
+// -------------------------------------------
+
+// ----------------Question 7-----------------------------
+// var book="A smarter way to learn JavaScript";
+// alert("I am trying to lern from the Book: "+book)
+// --------------------------------------------------------
+
+// ----------------Question 8-----------------------------------------
+// var text="Yeah! I can write HTML content through Java Script";
+// document.write(text);
+//---------------------------------------------------------------------
+
+// -----------------Question 9------------------------------------------
+// var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// ---------------------------------------------------------------------
+// -------------------------------------END------------------------------------------------------------------
+
+// -------------------------------------Chapter 3------------------------------------------------------------------
+
+// ------------------Question 1--------------------
+// var age=20;
+// alert("I am "+age+" Years old");
+// ------------------------------------------------
+
+// ------------------Question 2--------------------
+// initialCount=14;
+// count=initialCount+1;
+// alert("Total Visit Count= "+count);
+// ------------------------------------------------
+
+// ------------------Question 3--------------------
+// var birthYear=2002;
+// document.write("My Birth Year is "+birthYear);
+// ------------------------------------------------
+
+// ------------------Question 4--------------------
+// var cName="Jhon Doe", pTitle="T-Shirts", quantity=5;
+// document.write(cName+" Ordered "+quantity+" "+pTitle+" on XYZ Clothing Store");
+// ------------------------------------------------
+
+// -------------------------------------END------------------------------------------------------------------
+
+// -------------------------------------Chapter 4------------------------------------------------------------------
+// ------------------Question 1--------------------
+// var name="Brian", age=14, phoneNumber="021090078601";
+// ------------------------------------------------
+
+// ------------------Question 2--------------------
+// var name,phoneNumber,date_Birth, $currency,plyer1
+// var class-Number, ticket Number, alert, 5games, var 
+// ------------------------------------------------
+
+
+// ------------------Question 3--------------------
+// var first_Line="Rules for naming JS variables";
+// var second_Line="Variable names can only contain numbers, '$', and '_'. For Exmple: $my_1varible";
+// var third_Line="Variables must begin with a letter, '$' or '_'. For example $name, _name or name"
+// var fourth_Line="Variable names are case Sensitive"
+// var fifth_Line= "Variable names should not be JS Keywords"
+// document.write(first_Line+"<br>"+second_Line+"<br>"+third_Line+"<br>"+fourth_Line+"<br>"+fifth_Line);
+// ------------------------------------------------ 
+// -------------------------------------END------------------------------------------------------------------
