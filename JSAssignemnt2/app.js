d14f1ac901aab7a296961e2e4b2e2408d0be5a79
Muhammad-Ehtesham <shamiaurangzeb@gmail.com>
2023-10-22 13:27:15 +0500

    Assignment 2

---
+++ JSAssignemnt2/app.js
@@ -0,0 +1,97 @@
+// --------------CHAPTER 5----------------//
+
+// var x=10;
+// var y=6;
+// var z=x+y;
+
+// --------------Q1
+// document.write(x+" + "+y+" = "+z+"<br>");
+
+// --------------Q2
+// document.write(x+" - "+y+" = "+(x-y)+"<br>");
+// document.write(x+" * "+y+" = "+(x*y)+"<br>");
+// document.write(x+" / "+y+" = "+(x/y)+"<br>");
+// document.write(x+" % "+y+" = "+(x%y)+"<br>");
+
+// --------------Q3
+// var num;
+// document.write("Value after variable declaration is: "+num+"<br>");
+// num=20;
+// document.write("Initial Value: "+num+"<br>");
+// num++;
+// document.write("Value After Increment: "+num+"<br>");
+// num=num+7;
+// document.write("Value After Addition: "+num+"<br>");
+// num--;
+// document.write("Value After decrement: "+num+"<br>");
+// num=num%3;
+// document.write("The remainder after dividing by 3: "+num+"<br>");
+
+// --------------Q4
+// var price=600, tickets=5;
+// document.write("Price per ticket: "+price+" PKR <br>");
+// document.write("Number of tickets bought: "+tickets+" PKR <br>");
+// document.write("Total Cost: "+(price*tickets)+" PKR <br>");
+
+// --------------Q5
+// var table=3;
+// document.write("Table of " +table+"<br>");
+// for(var i=1;i<=10;i++){
+//     document.write(table+" X "+i+" = "+(table*i)+"<br>");
+// }
+
+// --------------Q6
+// var celsius=27,farenheit=99;
+// document.write(celsius+"C is "+((celsius*(9/5))+32)+"F <br>");
+// document.write(farenheit+"F is "+(((farenheit-32)*(5/9)))+"C <br>");
+
+// --------------Q7
+// var item1=500,item2=300,Q_it1=3,Q_it2=1;
+// var ship=250; tCost=0;
+// document.write("<h1>Shopping Cart</h1><br>");
+// document.write("Price of Item 1: "+item1+"<br>");
+// document.write("Quantity of Item 1: "+Q_it1+"<br>");
+// document.write("Price of Item 2: "+item2+"<br>");
+// document.write("Quantity of Item 2: "+Q_it2+"<br>");
+// document.write("Shipping Charges: "+ship+"<br>")
+// tCost=(item1*Q_it1)+(item2*Q_it2)+ship;
+// document.write("Total Cost of your Order is: "+tCost);
+
+// --------------Q8
+// var tMarks=500,oMarks=399,precentge=0;
+// precentge=(oMarks/tMarks)*100;
+// document.write("Total Marks: "+tMarks+"<br>Marks Obtined: "+oMarks+"<br>Percentge: "+precentge);
+
+// --------------Q9
+// var usd=10,riyal=25,pkr=0;
+// pkr=(usd*275.00)+(riyal*73.31);
+// document.write("<h1>Currency in PKR</h1><br>");
+// document.write("Total Currency in PKR: "+pkr+"Rs.");
+
+// --------------Q10
+// var x=0,y=0;
+// y=(((x+5)*10)/2)
+// document.write("Result: "+y);
+
+// --------------Q11
+// var CurrentYear=2023,BirthYear=2000;
+// document.write("<h1>Age Calculator</h1><br>Current Yer: "+CurrentYear+
+// "<br>Birth Year: "+BirthYear+"<br>Your Age is: "+(CurrentYear-BirthYear));
+
+// --------------Q12
+// var radius=7, circumfrence=0, area=0;
+// circumfrence=2*3.142*radius;
+// area=3.142*radius*radius;
+// document.write("<h1>The Geometrizer:</h1> <br>");
+// document.write("Radius of a Circle: "+radius+"<br>");
+// document.write("Circumference of a Circle: "+circumfrence+"<br>");
+// document.write("Area of a Circle: "+area+"<br>");
+
+// // --------------Q13
+// var snake="Ice-Craem",age=20,expectedAge=70,Q=2;
+// document.write("<h1>The Lifetime Supply Calculator:</h1><br>");
+// document.write("Favorite Snack: "+snake+"<br>");
+// document.write("Current Age: "+age+"<br>");
+// document.write("Estimated Maximum Age: "+expectedAge+"<br>");
+// document.write("Amount of Snakes per Day"+Q+"<br>");
+// document.write("“You will need"+((expectedAge-age)*365*Q)+" "+snake+"to last you until the ripe old age of "+expectedAge);
