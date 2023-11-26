094f37c72787eafb5d01a6959e0003013223f496
Muhammad-Ehtesham <shamiaurangzeb@gmail.com>
2023-11-26 12:47:25 +0500

    Assignment 3

---
+++ Array/app.js
@@ -0,0 +1,72 @@
+// --- Question 1-2 ---
+// var names=[];
+// names.push("Ehtesham");
+// console.log(names[0]);
+
+// --- Question 3 ---
+// var words=["Hello","There","Welcome"];
+
+// --- Question 4---
+// var x=[1,2,3,4,5,6];
+
+// --- Question 5 --- 
+// const booleanArr=["true","false"];
+// console.log(booleanArr)
+
+// --- Question 6 ---
+// var mixedArr=["Ehtesham",21,5.3];
+
+// --- Question 7 ---
+// const qualification=["SSC","HSC","BCS","BS","BCOM","MS","M.Phill","PhD"];
+// for(var i=0;i<8;i++)
+// {
+//     document.write((i+1)+") "+qualification[i]+"<br>");
+// }
+
+// --- Question 8 --- 
+// const names=["Abdullah","Faizan","Ammad"];
+// const score=[320,230,480];
+// for (var i=0;i<3;i++){
+//     document.write("Score of "+names[i]+" is "+score[i]+". Precentage: "+((score[i]/500)*100)+"%<br>");
+// }
+
+// --- Question 9 --- 
+// var color=["white","black","red","yellow","blue"]; 
+// color.push(prompt("A) Enter Color: "));
+// document.write("B) "+color.join(" , " )+"<br>");
+// color.unshift(prompt("Enter Color at begning: "));
+// color.unshift(prompt("Enter Color at begning: "));
+// document.write("C) "+color.join(" , " )+"<br>");
+// color.shift();
+// document.write("D) "+color.join(" , " )+"<br>");
+// color.pop();
+// document.write("E) "+color.join(" , " )+"<br>");
+// var index=prompt("Enter Index to add color: ");
+// color[index]=prompt("Ehter Color: ");
+// document.write("F) "+color.join(" , " )+"<br>"); 
+
+// var dindex=prompt("G) Enter Index to delete color: ");
+// delete color[dindex];
+// for(var i=1;i<=index;i++){
+//     color.pop();
+// }
+// document.write("G) "+color.join(" , " )+"<br>"); 
+
+// --- Question 10 --- 
+// var scores=[320,230,480,120];
+// document.write("Scorse sof students: "+scores.join(" , ")+"<br>");
+// scores.sort();
+// document.write("Ordered score sof students: "+scores.join(" , "));
+
+// --- Question 11 --- 
+// const cities=["Karachi","Haydrabad","Lahore","Islamabad","Multan"];
+// var selectedcities=[];
+// for(var i=0;i<3;i++){
+//     selectedcities[i]=cities[i];
+// }
+// document.write("Cities List: <br>"+cities.join(" , ")+"<br>");
+// document.write("<br>Selected cities List: <br>"+selectedcities.join(" , ")+"<br>");
+
+
+
+
