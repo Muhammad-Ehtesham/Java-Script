eb01b2a56751e4dff7f1f46c0658b676f3394bf7
Muhammad-Ehtesham <shamiaurangzeb@gmail.com>
2023-12-03 11:44:36 +0500

    Update app.js

--- task1/app.js
+++ task1/app.js
@@ -1,18 +1,29 @@
-var userName = prompt("Enter your username:");
-  var phoneNumber = prompt("Enter your phone number:");
-  var email = prompt("Enter your email address:");
-  var password = prompt("Enter your password:");
-  var confirmPassword = prompt("Confirm your password:");
+// var userName = prompt("Enter your username:");
+//   var phoneNumber = prompt("Enter your phone number:");
+//   var email = prompt("Enter your email address:");
+//   var password = prompt("Enter your password:");
+//   var confirmPassword = prompt("Confirm your password:");
 
-  if (!userName || !phoneNumber || !email || !password || !confirmPassword) {
-    alert("Please fill in all required fields.");
-  }
-  else{
-    if (password !== confirmPassword) {
-        alert("Passwords do not match. Please try again.");
-      }
-    else{
-        alert("You are successfully registered!");
-    }
-  }  
-  
+//   if (!userName || !phoneNumber || !email || !password || !confirmPassword) {
+//     alert("Please fill in all required fields.");
+//   }
+//   else{
+//     if (password !== confirmPassword) {
+//         alert("Passwords do not match. Please try again.");
+//       }
+//     else{
+//         alert("You are successfully registered!");
+//     }
+//   }  
+
+// --------------------Task 2-----------------------------
+const name=[];
+name[0]=prompt("Enter string: ");
+name[1]=[...name[0]].reverse().join("");
+console.log(name)
+if(name[0]===name[1]){
+    console.log("It's Plaindrom");
+}
+else{
+    console.log("It's not Plaindrom");
+}  
